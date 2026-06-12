3d5c89d3aee545675b3a1f12767d4910df62b6a7
Likhil Maiya <likhilnm17103@gmail.com>
2026-06-12 09:41:32 +0530

    tasks threads

---
+++ jsClassWork/lec1.js
@@ -0,0 +1,16 @@
+const dt=new Date();
+console.log(dt.getFullYear()  );
+
+var a="lik";
+var b="ma";
+
+console.log(a+" "+b);
+
+var age=17;
+if(age>=18){
+    console.log("you can vote");
+}else{
+    console.log("you cannot vote");
+}
+
+console.log(100/0);
